9f30c18af73e54ae4c598571c497c37c4330d04b
daseinkapital <drewbaer@vt.edu>
2021-01-16 15:19:59 -0800

    Removing bulky image from top

--- js/scripts.js
+++ js/scripts.js
@@ -135,11 +135,11 @@
 
     // Collapse Navbar
     var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
+        // if ($("#mainNav").offset().top > 00) {
+        $("#mainNav").addClass("navbar-shrink");
+        // } else {
+        //     $("#mainNav").removeClass("navbar-shrink");
+        // }
     };
     // Collapse now if page is not at top
     navbarCollapse();
